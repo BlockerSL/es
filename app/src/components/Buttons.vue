836d1617930c8cd36e3c0087a3d11e13865f9774
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-09-12 17:40:44 +0300

    Update Buttons.vue

--- app/src/components/Buttons.vue
+++ app/src/components/Buttons.vue
@@ -1,11 +1,12 @@
+
 <template>
   <div>
     <el-button
       type="primary"
       icon="el-icon-refresh"
       @click="random"
-    >Solve هاوکێشەی هەڕەمەکی </el-button>
-   <el-button
+    >Solve random equation</el-button>
+    <el-button
       type="primary"
       icon="el-icon-view"
       @click="syntaxGuide = !syntaxGuide"
@@ -13,15 +14,12 @@
       {{ syntaxGuide ? "Hide" : "Show" }} syntax guide
     </el-button>
     <SyntaxGuide :initialVisibility="syntaxGuide" @visibility="syntaxGuide = $event" />
-
-
   </div>
 </template>
 
 <script>
 import SyntaxGuide from './SyntaxGuide.vue';
 import { random as generateEquation } from '../library/chem-balancer';
-
 export default {
   name: 'Buttons',
   components: { SyntaxGuide },
